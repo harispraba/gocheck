55930fbf0528a59fed48977e8109f36c7b2bcfeb
Abdur Rofi <rofim58@gmail.com>
2021-09-23 08:23:31 +0700

    Update fiture check available domain

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module gocheck
 
 go 1.17
 
-require github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.7.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+	github.com/domainr/whois v0.0.0-20210920161046-bc5979f7911c // indirect
+	github.com/haccer/available v0.0.0-20200921032034-0d552efb6cd4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/zonedb/zonedb v1.0.3380 // indirect
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/haccer/available"
 )
 
 var (
@@ -18,27 +20,29 @@ var (
 )
 
 func checkSSL(domain string, webhookURL string) {
-	conn, err := tls.Dial("tcp", domain+":443", nil)
-	if err != nil {
-		panic("Server doesn't support SSL certificate err: " + err.Error())
-	}
-	err = conn.VerifyHostname(domain)
-	if err != nil {
-		panic("Hostname doesn't match with certificate: " + err.Error())
-	}
-	expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
-	date := expiry
-	currentTime := time.Now()
-	expiredDays := int(date.Sub(currentTime).Hours() / 24)
-	fmt.Printf("Dommain: %s\nIssuer: %s\nExpiry Date: %v\nDays: %v day\n=================\n", domain, conn.ConnectionState().PeerCertificates[0].Issuer, date.Format(time.RFC850), expiredDays)
-	if expiredDays < 30 {
-		m := map[string]string{"content": "Hello @everyone, domain " + domain + " mau expired nih tanggal " + date.Format("24-08-2001")}
-		r, w := io.Pipe()
-		go func() {
-			json.NewEncoder(w).Encode(m)
-			w.Close()
-		}()
-		http.Post(webhookURL, "application/json", r)
+	if !available.Domain(domain) {
+		conn, err := tls.Dial("tcp", domain+":443", nil)
+		if err != nil {
+			panic("Server doesn't support SSL certificate err: " + err.Error())
+		}
+		err = conn.VerifyHostname(domain)
+		if err != nil {
+			panic("Hostname doesn't match with certificate: " + err.Error())
+		}
+		expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+		date := expiry
+		currentTime := time.Now()
+		expiredDays := int(date.Sub(currentTime).Hours() / 24)
+		fmt.Printf("Dommain: %s\nIssuer: %s\nExpiry Date: %v\nDays: %v day\n=================\n", domain, conn.ConnectionState().PeerCertificates[0].Issuer, date.Format(time.RFC850), expiredDays)
+		if expiredDays < 30 {
+			m := map[string]string{"content": "Hello @everyone, domain " + domain + " mau expired nih tanggal " + date.Format(time.RFC822)}
+			r, w := io.Pipe()
+			go func() {
+				json.NewEncoder(w).Encode(m)
+				w.Close()
+			}()
+			http.Post(webhookURL, "application/json", r)
+		}
 	}
 }
 func main() {
